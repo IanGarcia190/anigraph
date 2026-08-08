dfec61d9a05ea476b23f302f8ce1ffb782d1053b
Ian <ian667217@gmail.com>
2026-08-08 11:57:28 -0600

    Centered Axis Labels, acquired text for numbers along axis. Want to get positions for numbers on screen and draw on the graph.

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,21 @@ fn window() -> Conf {
 //-------------Main Loop--------------------------------------------------------
 #[macroquad::main(window)]
 async fn main() {
+    let x_range = make_range(0.0, 8.0, 1.0);
+    let data_points = linear_function(&x_range);
+    let (x_dim, y_dim) = get_dimensions(&data_points);
+
+    for x in x_dim {
+        println!("{}", x);
+    }
+    println!();
+    for y in y_dim {
+        println!("{}", y);
+    }
+
     loop {
         clear_background(BLACK);
-        draw_axis();
+        draw_axis(&data_points);
         if is_key_pressed(KeyCode::Q) {
             break;
         }
@@ -27,51 +39,130 @@ async fn main() {
     }
 }
 
-fn draw_axis() {
+fn linear_function(range: &[f32]) -> Vec<Vec2> {
+    let mut points: Vec<Vec2> = Vec::new();
+    for x in range {
+        let y = *x;
+        points.push(Vec2::new(*x, y));
+    }
+
+    points
+}
+
+fn get_dimensions(points: &[Vec2]) -> (Vec<f32>, Vec<f32>) {
+    let mut x_range = Vec::new();
+    let mut y_range = Vec::new();
+
+    for point in points {
+        x_range.push(point.x);
+        y_range.push(point.y);
+    }
+
+    //sus out actual dimensions that these positions give on the body of the graph;
+    //find max, divide by max, multiply every element by body width.
+
+    let bodywidth = 0.8 * screen_width();
+    let bodyheight = 0.8 * screen_height();
+    let x_max = f32max(&x_range);
+    let y_max = f32max(&y_range);
+
+    for x in x_range.iter_mut() {
+        *x /= x_max;
+        *x *= bodywidth;
+    }
+
+    for y in y_range.iter_mut() {
+        *y /= y_max;
+        *y *= bodyheight;
+    }
+
+    (x_range, y_range)
+}
+
+fn draw_axis(points: &Vec<Vec2>) {
     //I am going to do arithmetic in terms of percentile.
     //So I can simply multiply by window size.
 
+    //y-axis part of frame
     draw_rectangle(
         0.1 * screen_width(),
         0.1 * screen_height(),
-        0.01 * screen_width(),
+        2.0,
         0.8 * screen_height(),
         YELLOW,
     );
 
+    //x-axis part of frame
     draw_rectangle(
         0.1 * screen_width(),
         0.9 * screen_height(),
         0.8 * screen_width(),
-        0.01 * screen_height(),
+        2.0,
         YELLOW,
     );
 
-    let mut text = "X-Axis";
-    let color = YELLOW;
-    let mut center_offset = get_text_center(text, None, 30, 1.0, 0.0);
-    let rotation = -90.0f32.to_radians();
-
-    draw_text(
-        text,
-        (screen_width() / 2.0) - center_offset.x,
+    text_draw(
+        "X-Axis",
+        screen_width() / 2.0,
         screen_height() * 0.95,
-        30.0,
-        color,
+        None,
+        30,
+        0.0,
+        YELLOW,
     );
 
-    text = "Y-Axis";
-    center_offset = get_text_center(text, None, 30, 1.0, rotation);
+    text_draw(
+        "Y-Axis",
+        screen_width() * 0.05,
+        screen_height() / 2.0,
+        None,
+        30,
+        -90.0f32.to_radians(),
+        YELLOW,
+    );
+}
+
+//The x and y target are the target postions for the center of the text box.
+fn text_draw(
+    text: &str,
+    x_target: f32,
+    y_target: f32,
+    font: Option<&Font>,
+    font_size: u16,
+    rotation: f32,
+    color: Color,
+) {
+    let center_offset = get_text_center(text, font, font_size, 1.0, rotation);
 
     draw_text_ex(
         text,
-        screen_width() * 0.05,
-        (screen_height() / 2.0) - center_offset.y,
+        x_target - center_offset.x,
+        y_target - center_offset.y,
         TextParams {
-            font_size: 30,
+            font_size,
             rotation,
             color,
             ..Default::default()
         },
     );
+}
+
+fn make_range(start: f32, stop: f32, step: f32) -> Vec<f32> {
+    let mut range = Vec::new();
+    let mut current = start;
+    while current < stop {
+        range.push(current);
+        current += step;
+    }
+    range
+}
+
+fn f32max(range: &[f32]) -> f32 {
+    let mut max = 0.0;
+    for num in range {
+        if *num > max {
+            max = *num;
+        }
+    }
+    max
 }
